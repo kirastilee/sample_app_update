9368d0e8b52ccc4b52fed79cb2aa61b99d38ca70
khang <kira.stilee@gmail.com>
2021-01-20 20:30:50 +0700

    static_flling

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,26 @@
 const { environment } = require('@rails/webpacker')
 
+const less_loader= {
+  test: /\.less$/,
+  use: ['css-loader', 'less-loader']
+ };
+ environment.loaders.append('less', less_loader)
+
+const webpack = require('webpack')
+
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery'
+  })
+)
+
+environment.loaders.append("jquery", {
+  test: require.resolve("jquery"),
+  use: [
+    { loader: "expose-loader", options: { exposes: ["$", "jQuery"] } },
+  ],
+});
+
 module.exports = environment
