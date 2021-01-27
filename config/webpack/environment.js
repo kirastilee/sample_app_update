74afba19181feead8cb0e158089e6ee1a87f8b6d
khang <kira.stilee@gmail.com>
2021-01-27 21:10:27 +0700

    chap13

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+  environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+  )
+
 const less_loader= {
   test: /\.less$/,
   use: ['css-loader', 'less-loader']
